5df9b71ac846a2cddc8c5d98a0cec2909cbec886
RodrigoGomes00 <digojales@hotmail.com>
2023-09-12 16:07:27 -0300

    Index Route

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,18 @@ import Expenses from './routes/Expenses/'
 import Invoices from './routes/Invoices/'
 import NotFound from './routes/NotFound/index.tsx'
 import Invoice from './routes/Invoices/invoice/index.tsx'
+import InvoicesIndex from './routes/Invoices/InvoicesIndex/index.tsx'
+import HomePage from './routes/HomePage/index.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} >
+      <Route index element={<HomePage />} />
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route index element={<InvoicesIndex />} />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route >
         <Route path="*" element={<NotFound />} />
---
+++ src/routes/HomePage/index.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+
+    return (
+        <main>
+           <h1>Bem-Vindo!</h1>
+        </main>
+    );
+}
