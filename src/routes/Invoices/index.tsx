e61a28dd4c4c1a96a5e3dc42f57ec46991bc83a5
RodrigoGomes00 <digojales@hotmail.com>
2023-09-12 16:45:38 -0300

    Search Params

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,21 +1,44 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
 import './style.css';
 
 export default function Invoices() {
 
     const invoices = getInvoices();
+    let [searchParams, setSearchParams] = useSearchParams();
 
 
     return (
         <div style={{ display: "flex" }}>
-            <nav
-                style={{
-                    borderRight: "solid 1px",
-                    padding: "1rem",
-                }}
-            >
-                {invoices.map((invoice) => (
+            <nav style={{ borderRight: "solid 1px", padding: "1rem" }} >
+
+                <div>
+                    <input
+                        value={searchParams.get("name") || ""}
+                        onChange={(event) => {
+                            const name = event.target.value;
+                            if (name) {
+                                setSearchParams({ name });
+                            } else {
+                                setSearchParams({});
+                            }
+                        }}
+                    />
+                </div>
+
+
+                {invoices
+
+                .filter((invoice) => {
+                    let name = searchParams.get("name");
+                    if (!name) {
+                        return true;
+                    }
+                    let invoiceName = invoice.name.toLowerCase();
+                    return invoiceName.startsWith(name.toLowerCase());
+                  })
+
+                .map((invoice) => (
                     <NavLink
                         className={({ isActive }) => isActive ? "nav-red" : "nav-blue"}
                         style={{ display: "block", margin: "1rem 0" }}
@@ -27,7 +50,7 @@ export default function Invoices() {
                 ))}
             </nav>
 
-            <Outlet/>
+            <Outlet />
         </div>
     );
 }
